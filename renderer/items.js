4a86d61ea15987f87f717a9c9d4e3ba5b6264635
Your Name <swietonp@icloud.com>
2023-09-24 12:20:30 +0200

    New functionality: deleting items after clicking 'Done'

--- renderer/items.js
+++ renderer/items.js
@@ -13,6 +13,59 @@ fs.readFile(`${__dirname}/reader.js`, (err, data) => {
 // Track items in storage
 exports.storage = JSON.parse(localStorage.getItem('readit-items')) || []
 
+// Listen for 'done' message from reader window
+window.addEventListener('message', e => {
+
+  // Check if corrent message
+  if (e.data.action === 'delete-reader-item') {
+
+      // Delete item at given index
+      this.delete(e.data.itemIndex)
+
+      // Close the reader window
+      e.source.close()
+  }
+})
+
+// Delete item
+exports.delete = itemIndex => {
+
+  // Remove item from DOM
+  items.removeChild( items.childNodes[itemIndex] )
+
+  // Remove item from storage
+  this.storage.splice(itemIndex, 1)
+
+  // Persist storage
+  this.save()
+
+  // Select previous item or new top item
+  if (this.storage.length) {
+
+    // Get new selected item index
+    let = newSelectedItemIndex = (itemIndex === 0) ? 0 : itemIndex - 1
+
+    // Select item at new index
+    document.getElementsByClassName('read-item')[newSelectedItemIndex].classList.add('selected')
+  }
+}
+
+// Get selected item index
+exports.getSelectedItem = () => {
+
+  // Get selected node
+  let currentItem = document.getElementsByClassName('read-item selected')[0]
+
+  // Get item index
+  let itemIndex = 0
+  let child = currentItem
+  while ( (child = child.previousElementSibling) != null ) itemIndex++
+
+  // Return selected item and index
+  return { node: currentItem, index: itemIndex }
+
+}
+
 // Persist storage
 exports.save = () => {
   localStorage.setItem('readit-items', JSON.stringify(this.storage))
@@ -22,7 +75,7 @@ exports.save = () => {
 exports.select = e => {
 
   // Remove currently selected item class
-  document.getElementsByClassName('read-item selected')[0].classList.remove('selected')
+  this.getSelectedItem().node.classList.remove('selected')
 
   // Add to clicked item
   e.currentTarget.classList.add('selected')
@@ -32,16 +85,16 @@ exports.select = e => {
 exports.changeSelection = direction => {
 
   // Get selected item
-  let currentItem = document.getElementsByClassName('read-item selected')[0]
+  let currentItem = this.getSelectedItem()
 
   // Handle up/down
   if (direction === 'ArrowUp' && currentItem.previousElementSibling) {
-    currentItem.classList.remove('selected')
-    currentItem.previousElementSibling.classList.add('selected')
+    currentItem.node.classList.remove('selected')
+    currentItem.node.previousElementSibling.classList.add('selected')
 
   } else if (direction === 'ArrowDown' && currentItem.nextElementSibling) {
-    currentItem.classList.remove('selected')
-    currentItem.nextElementSibling.classList.add('selected')
+    currentItem.node.classList.remove('selected')
+    currentItem.node.nextElementSibling.classList.add('selected')
   }
 }
 
@@ -52,10 +105,10 @@ exports.open = () => {
   if ( !this.storage.length ) return
 
   // Get selected item
-  let selectedItem = document.getElementsByClassName('read-item selected')[0]
+  let selectedItem = this.getSelectedItem()
 
   // Get item's url
-  let contentURL = selectedItem.dataset.url
+  let contentURL = selectedItem.node.dataset.url
 
   // Open item in proxy BrowserWindow
   let readerWin = window.open(contentURL, '', `
@@ -69,7 +122,7 @@ exports.open = () => {
   `)
 
   // Injest JS
-  readerWin.eval(readerJS)
+  readerWin.eval( readerJS.replace('{{index}}', selectedItem.index) )
 }
 
 // Add new item
