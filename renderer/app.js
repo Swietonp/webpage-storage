a346a532d21e7e9455b03ae3f648adc5957831a2
Your Name <swietonp@icloud.com>
2023-09-24 14:42:42 +0200

    Application Menu added

--- renderer/app.js
+++ renderer/app.js
@@ -1,3 +1,4 @@
+
 // Modules
 const {ipcRenderer} = require('electron')
 const items = require('./items')
@@ -9,6 +10,32 @@ let showModal = document.getElementById('show-modal'),
     addItem = document.getElementById('add-item'),
     itemUrl = document.getElementById('url'),
     search = document.getElementById('search')
+
+// Open modal from menu
+ipcRenderer.on( 'menu-show-modal', () => {
+  showModal.click()
+})
+
+// Open selected item from menu
+ipcRenderer.on( 'menu-open-item', () => {
+  items.open()
+})
+
+// Delete selected item from menu
+ipcRenderer.on( 'menu-delete-item', () => {
+  let selectedItem = items.getSelectedItem()
+  items.delete(selectedItem.index)
+})
+
+// Open item in native browser from menu
+ipcRenderer.on( 'menu-open-item-native', () => {
+  items.openNative()
+})
+
+// Focus the search input from the menu
+ipcRenderer.on( 'menu-focus-search', () => {
+  search.focus()
+})
 
 // Filter items with "search"
 search.addEventListener('keyup', e => {
